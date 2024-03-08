789727bac0f23ad8095d1c4eb8f105f1aeb2561e
s0913191 <s0913191@gmail.com>
2024-03-07 21:38:46 -0500

    single link

--- submission.py
+++ submission.py
@@ -3,6 +3,19 @@ from typing import List
 # you may use other Python standard libraries, but not data
 # science libraries, such as numpy, scikit-learn, etc.
 
+def dist(a: List[float], b: List[float]):
+    return ((a[0]-b[0])**2 + (a[1]-b[1])**2)**(1/2)
+
+def reindex_clusters(clusters):
+    unique_clusters = set(sorted(clusters))
+    normalized_clusters = set([i for i in range(len(set(clusters)))])
+    before_clusters = unique_clusters.difference(normalized_clusters)
+    after_clusters = normalized_clusters.difference(unique_clusters)
+
+    for b, a in zip(before_clusters, after_clusters):
+        clusters = [(a if (c == b) else c) for c in clusters]
+    return clusters
+
 class Solution:
   def hclus_single_link(self, X: List[List[float]], K: int) -> List[int]:
     """Single link hierarchical clustering
@@ -14,9 +27,31 @@ class Solution:
       The number does not matter as long as the clusters are correct.
       For example: [0, 0, 1] is treated the same as [1, 1, 0]"""
     # implement this function
-    print('test')
-    pass
 
+    N = len(X)
+    dist_matrix = List[List[float]]
+    dist_matrix = [[dist(X[i], X[j]) for i in range(N)] for j in range(N)]
+
+    # Initialize clusters
+    clusters = List[int]
+    clusters = [i for i in range(N)]
+
+    while len(set(clusters)) > K:
+        # Calculate the minimum distance
+        min_dist = float('inf')
+        for i in range(N):
+            for j in range(N):
+                if clusters[i] == clusters[j]:
+                    continue
+                if (dist_matrix[i][j] <= min_dist) and (dist_matrix[i][j] != 0):
+                    min_dist = dist_matrix[i][j]
+                    min_dist_a = i
+                    min_dist_b = j
+
+        clusters[min_dist_a] = clusters[min_dist_b]
+        clusters = reindex_clusters(clusters)
+    return clusters
+  
   def hclus_average_link(self, X: List[List[float]], K: int) -> List[int]:
     """Complete link hierarchical clustering"""
     # implement this function
---
+++ test.py
@@ -0,0 +1,27 @@
+from submission import Solution
+from typing import List
+
+input_path = 'sample_test_cases/input00.txt'
+output_path = 'sample_test_cases/output00.txt'
+
+with open(input_path, 'r') as f:
+    data = [tuple(float(e) for e in s.rstrip('\n').split(' ')) for s in f.readlines()]
+    N = int(data[0][0])
+    K = int(data[0][1])
+    M = int(data[0][2])
+    data = data[1:]
+
+with open(output_path, 'r') as f:
+    validate_clusters = [int(s.rstrip('\n')) for s in f.readlines()]
+
+
+clustering = Solution()
+clusters = clustering.hclus_single_link(data, K)
+
+
+print(clusters, validate_clusters)
+
+
+
+
+
